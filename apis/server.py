c1768e30204594888f47b351c9ce40a3f7b60967
MT <04x0xx@gmail.com>
2024-04-12 18:17:36 -0300

    Added initial configuration

---
+++ apis/server.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from flask import Flask
+
+if __name__ == "__main__":
+    ...
---
+++ src/app.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const app = () => {
+    return (
+        <p>Hello Wrld!</p>
+    );
+}
+
+export default app;
